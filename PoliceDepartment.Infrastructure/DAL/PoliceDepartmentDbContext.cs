3278dc9d5990c0007b9ab82034d74951f456af5c
b-commits <s19677@pjwstk.edu.pl>
2024-04-13 20:21:11 +0200

    PD-0 Added AzureOnPremiseMySqlConnectionStringNormalizer.cs

--- PoliceDepartment.Infrastructure/DAL/PoliceDepartmentDbContext.cs
+++ PoliceDepartment.Infrastructure/DAL/PoliceDepartmentDbContext.cs
@@ -1,11 +1,11 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using PoliceDepartment.Core.Entities;
+using PoliceDepartment.Infrastructure.Helpers;
 using PoliceDepartment.Infrastructure.Interceptors;
 
 namespace PoliceDepartment.Infrastructure.DAL;
@@ -62,21 +62,12 @@ internal class PoliceDepartmentDbContext : DbContext
         optionsBuilder.AddInterceptors(new AuditableEntityInterceptor(_auditLogger));
 
         var connection = _environment.IsProduction()
-            ? Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb")
+            ? AzureOnPremiseMySqlConnectionStringNormalizer.Normalize(Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb"))
             : _configuration.GetConnectionString(connectionStringSection);
 
         _logger.LogInformation("{name} is {connectionString}.", 
             nameof(connection), connection);
         
-        string dbhost = Regex.Match(connection, @"Data Source=(.+?);").Groups[1].Value;
-        string server = dbhost.Split(':')[0].ToString();
-        string port = dbhost.Split(':')[1].ToString();
-        string dbname = Regex.Match(connection, @"Database=(.+?);").Groups[1].Value;
-        string dbusername = Regex.Match(connection, @"User Id=(.+?);").Groups[1].Value;
-        string dbpassword = Regex.Match(connection, @"Password=(.+?)$").Groups[1].Value;
-        
-        _logger.LogInformation("{A} {B} {C} {D} {E} {F} {G} {H}", dbhost, server, port, dbname, dbusername, dbpassword);
-
         if (string.IsNullOrEmpty(connection))
         {
             _logger.LogWarning("Connection string for {connectionString} could not be found. Please verify the appSettings.", 
---
+++ PoliceDepartment.Infrastructure/Helpers/AzureOnPremiseMySqlConnectionStringNormalizer.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.Logging;
+
+namespace PoliceDepartment.Infrastructure.Helpers;
+
+internal static class AzureOnPremiseMySqlConnectionStringNormalizer
+{
+    internal static string Normalize(string? rawAzureOnPremiseMySqlConnectionString) {
+        var dictionary = rawAzureOnPremiseMySqlConnectionString!
+                .Split(';')
+                .Where(keyValuePair => keyValuePair.Contains('='))
+                .Select(kvp => kvp.Split(['='], 2))
+                .ToDictionary(kvp => kvp[0].Trim(), kvp => kvp[1].Trim(), StringComparer.InvariantCultureIgnoreCase);
+        var dataSource = dictionary["Data Source"];
+        var dataSourceTokens = dataSource.Split(":");
+        return $"Server={dataSourceTokens[0]};Port={dataSourceTokens[1]};Database={dictionary["Database"]};Uid={dictionary["User Id"]};Pwd={dictionary["Password"]};";
+    }
+}
